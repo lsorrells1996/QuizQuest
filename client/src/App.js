b4936ca02f0e722a58ad3d15d3b51ed5a3ea954a
Lorenzo Sorrells <lsorrells1996@gmail.com>
2022-03-24 14:29:11 -0500

    created login form

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,14 @@ import Landing from "./Components/Landing";
 
 
 function App() {
-  return (
+   
+    return (
     <BrowserRouter>
         <Routes>
-            <Route path= "/login" element={<Landing/>}/>
+            <Route path= "/login" element={<Landing />}/>
         </Routes>
     </BrowserRouter>
-  );
+    );
 }
 
 export default App;
--- client/src/Components/Landing.js
+++ client/src/Components/Landing.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Landing() {
-  return (
-    <div>Landing</div>
-  )
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    // const [createUsername, setCreateUsername] = useState('')
+    // const [createPassword, setCreatePassword] = useState('')
+    // const [email, setEmail] = useState('')
+
+    const onSubmit = e => {
+        e.prevent.default()
+        fetch('localhost:3000/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username,
+                password
+            }),
+        }).then(r => r.json()).then()
+    }
+
+    return (
+    <div>
+        <form onSubmit={onSubmit}>
+            <label>Username
+                <input onChange={(e) => setUsername(e.target.value)} type="text" placeholder="Username..."/>
+            </label>
+            <label>Password
+                <input onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password..."/>
+            </label>
+            <button type="submit">submit</button>
+        </form>
+    </div>
+    )
 }
 
 export default Landing
