4b4718d1b2cbffacd280868729817c650a987a51
Jonathan Bryant <spectrum19tech@gmail.com>
2022-03-25 13:13:06 -0700

    Refactor NavBar

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,33 +1,15 @@
 import React, {useState} from 'react'
 import { useNavigate } from "react-router-dom"
-
+import NavBar from './NavBar'
 
 function Home( {setUser} ) {
     const navigate = useNavigate()
 
-    const onLogout = () => {
-        fetch('/logout', {
-            method: 'DELETE'
-        }).then(() => setUser(null))
-        navigate('/')
-    }
+
 
     return (
         <>
-            <nav class="navbar navbar-light bg-secondary">
-                <ul class="nav">
-                    <li className='nav-item'>
-                        <a class="navbar-brand" href="#">
-                        <img src="https://res.cloudinary.com/dhaek7qxl/image/upload/v1648183355/Book-icon_anzcyj.jpg" alt="" width="30" height="24" className='mx-2'></img>
-                        QuizTown
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link link-dark" href="#">Link</a>
-                    </li>
-                </ul>
-                <button onClick={onLogout} className="me-5">Logout</button>
-            </nav>
+            <NavBar setUser={setUser}/>
         </>
     )
 
---
+++ client/src/Components/NavBar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useNavigate } from "react-router-dom"
+
+function NavBar({setUser}) {
+    const navigate = useNavigate()
+
+    const onLogout = () => {
+    fetch('/logout', {
+        method: 'DELETE'
+    }).then(() => setUser(null))
+    navigate('/')
+}
+    
+    return (
+    <nav class="navbar navbar-light bg-secondary">
+        <ul class="nav">
+            <li className='nav-item'>
+                <a class="navbar-brand" href="#">
+                <img src="https://res.cloudinary.com/dhaek7qxl/image/upload/v1648183355/Book-icon_anzcyj.jpg" alt="" width="30" height="24" className='mx-2'></img>
+                QuizTown
+                </a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link link-dark" href="#">Link</a>
+            </li>
+        </ul>
+        <button onClick={onLogout} className="me-5">Logout</button>
+    </nav>
+  )
+}
+
+export default NavBar
