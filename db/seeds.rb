a7f5128a752b5ad056e411b04fa68fc9ffbc3b24
Jonathan Bryant <spectrum19tech@gmail.com>
2022-03-25 08:29:49 -0700

    Started seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,17 @@
+User.all.destroy_all
+UserQuiz.all.destroy_all
+Quiz.all.destroy_all
+Question.all.destroy_all
+Answer.all.destroy_all
+
 puts "👤 Seeding Users..."
+10.times {
+    User.create(
+        username: Faker::Name.first_name
+    )
+}
+
+
 User.create(username: 'Jim', password: '123')
 
 
