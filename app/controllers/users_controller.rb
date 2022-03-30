a52a916f0288a6f0370b3085187e909a18d01797
Lorenzo Sorrells <lsorrells1996@gmail.com>
2022-03-30 10:12:15 -0500

    stats completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
     
     def create
         user = User.create!(user_params)
+        UserQuiz.create!(user_id: user.id, quiz_id: 1, score: 0)
+        UserQuiz.create!(user_id: user.id, quiz_id: 2, score: 0)
+        UserQuiz.create!(user_id: user.id, quiz_id: 3, score: 0)
+        UserQuiz.create!(user_id: user.id, quiz_id: 4, score: 0)
         session[:user_id] = user.id
         render json: user, status: :created
     end
